f2e556ca275e26494d0c0fbcad4250ebb21daebb
carolina barboza <carolina.barboza367@gmail.com>
2019-01-09 22:40:54 +0000

    i added a linked page to create an account

--- flask_app.py
+++ flask_app.py
@@ -3,8 +3,14 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def main():
-    # found in ../templates/
+    # found in ../templates/                                 #log in
     return render_template("main_page.html")
+
+@app.route('/create_account', methods=['GET'])
+def create_account():                                            #create account
+    # found in ../templates/
+    return render_template("create_account.html")
+
 
 @app.route('/process_inputs', methods=['POST'])
 def process_inputs():
@@ -13,3 +19,4 @@ def process_inputs():
     select = request.form.get('officer_select', '')
     return render_template("main_page.html", input_data=dropdown,
                            output="You're a wizard %s." % name)
+
